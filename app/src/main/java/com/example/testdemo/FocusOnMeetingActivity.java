ae3c0877f81a38e4416f5bd268f8f0cb2d45107f
Leif <17205995@ucdconnect.ie>
2019-04-19 19:18:21 +0800

    add some new activities

---
+++ app/src/main/java/com/example/testdemo/FocusOnMeetingActivity.java
@@ -0,0 +1,13 @@
+package com.example.testdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FocusOnMeetingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_focus_on_meeting);
+    }
+}
