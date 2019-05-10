02ab348e39e3ef5d81116a7311913d5484a9a906
Leif <17205995@ucdconnect.ie>
2019-05-10 08:40:26 +0800

    Change DirectionActivity To DictionaryActivity

--- app/src/main/java/com/example/testdemo/DictionaryActivity.java
+++ app/src/main/java/com/example/testdemo/DictionaryActivity.java
@@ -3,11 +3,11 @@ package com.example.testdemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class DirectionActivity extends AppCompatActivity {
+public class DictionaryActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_direction);
+        setContentView(R.layout.activity_dictionary);
     }
 }
